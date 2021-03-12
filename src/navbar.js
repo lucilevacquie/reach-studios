663247ce054b6ecd652d1bd7300a3a00e645f487
Lucile Vacquie <lucilevacquie@Luciles-MacBook-Pro.local>
2021-03-12 18:21:23 +0000

    navbar done

--- src/navbar.js
+++ src/navbar.js
@@ -1,17 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 import BBCLogo from "./assets/BBC_logo.png";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import SearchIcon from "@material-ui/icons/Search";
 
 const data = [
   {
     id: "Sign in",
     src: "",
     href: "/",
+    icon: <AccountCircleIcon />,
   },
   {
-    id: "bell",
+    id: "",
     src: "",
     href: "/",
+    icon: <NotificationsIcon />,
   },
   {
     id: "Home",
@@ -57,29 +63,54 @@ const data = [
     id: "More",
     src: "",
     href: "/",
+    icon: <ArrowDropDownIcon />,
   },
   {
     id: "Search",
     src: "",
     href: "/",
+    icon: <SearchIcon />,
   },
 ];
 
 const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(14, auto);
+  border-bottom: solid gray 1px;
 `;
 
-const Logo = styled.div``;
+const Box = styled.a`
+  display: flex;
+  align-items: center;
+  border-right: solid gray 1px;
+  justify-content: center;
+  padding: 7px;
+`;
+
+const LogoImgWrapper = styled.div`
+  width: 5.25rem;
+  height: 1.5rem;
+`;
+
+const LogoImg = styled.img`
+  width: 100%;
+  height: 100%;
+`;
 
 const Navbar = () => {
   return (
     <Container>
-      <Logo>
-        <button>
-          <img src={BBCLogo} alt="CBB logo" />
-        </button>
-      </Logo>
+      <Box>
+        <LogoImgWrapper>
+          <LogoImg src={BBCLogo} alt="CBB logo" />
+        </LogoImgWrapper>
+      </Box>
+      {data.map((item) => (
+        <Box key={item.id}>
+          {item.icon}
+          {item.id}
+        </Box>
+      ))}
     </Container>
   );
 };
